ee56f5b38e8e5d9abd67d22d5cb05edc37177609
nathaphat t. <76417777+putt-t@users.noreply.github.com>
2025-06-03 00:10:38 -0700

    refactor: remove shortest path test script and clean up unused query classes

--- helper-docs/src/app/(home)/page.tsx
+++ helper-docs/src/app/(home)/page.tsx
@@ -2,7 +2,7 @@
 export default function HomePage() {
     return (
         <main className="flex flex-1 flex-col justify-center text-center">
-
+            <p>built for internal use within netwrxk, but feel free to use it as a reference for your own use</p>
         </main>
     );
 }
